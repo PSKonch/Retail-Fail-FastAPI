979ec3a7b9c03b0c624b0a7d1fefd217ccc061e8
Dimitriy Shaban <shaban.benito.dimitri@gmail.com>
2025-01-20 14:12:03 +0300

    docker-v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-alembic upgrade head
-uvicorn src.main:app --reload --host 0.0.0.0 --port 8000
 
-wait
+# Ожидание запуска базы данных. Контейнер с базой данных не должен развернуться позже срабатывания данной команды
+echo "Waiting for database to be ready..."
+while ! nc -z ${POSTGRES_HOST} ${POSTGRES_PORT}; do
+  sleep 3
+done
+echo "Database is ready."
+
+# Применение миграций
+echo "Applying database migrations..."
+alembic upgrade head
+
+# Запуск приложения
+echo "Starting application..."
+uvicorn src.main:app --reload --host 0.0.0.0 --port 8000
